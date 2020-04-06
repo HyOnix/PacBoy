ab50734d9af911d4bc1a2931f7c6f842e4c16313
BrSkat <50581350+BrSkat@users.noreply.github.com>
2020-04-06 21:09:29 +0200

    generation de map a finioler pour que pac et le phantome ne naisse pas dans des murs

--- h/mes_fonctions.h
+++ h/mes_fonctions.h
@@ -11,6 +11,7 @@
 #define LargeurFenetre 600
 #define HauteurFenetre 800
 
+<<<<<<< Updated upstream
 
 
 /**
@@ -163,3 +164,39 @@ void Enregistrement(FILE *FileMap, FILE *FileScore, GameStats stats, entity 
  * \param[in] *FileScore : Fichier ou enregistrer le score
  */
 void NewScore(int score, FILE *FileScore)
+=======
+void GenMap();
+int rand_a_b(int a, int b);
+
+typedef struct {
+  int x;
+  int y;
+  int v;
+  int d;
+  int state;
+} Entity;
+
+typedef struct {
+  int point;
+  int vie;
+  int etat;
+  int vul;
+} GameStat;
+
+int min(int a, int b);
+
+void DeplacementPac(Entity *pac, char coef[32][29]);
+void InitEntity(Entity *ent, int x, int y, int v, int d, int s);
+void Manger(Entity pac, GameStat *stat, int taille, char coef[32][29]);
+void AffichageScore(int x, int y, GameStat stat);
+void AfficheVie(int x, int y, GameStat stat);
+
+void arrondiBout(int x, int y, int taille, int orientation);
+void arrondiAngle(int x, int y, int taille, int orientation);
+void FillMap(char coef[32][29]);
+void Map(char map[32][29], int x, int y);
+void DrawPac(float x, float y, int dx, int dy, int direction);
+void tri_angle(float x, float y, int ouverture, int direction, float taille);
+
+void DeplacementIA0(Entity *pac, char coef[32][29]);
+>>>>>>> Stashed changes
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 
+<<<<<<< Updated upstream
 #include "h/mes_fonctions.h"
 
 
@@ -9,6 +10,40 @@ int main(int argc, char **argv)
 	lanceBoucleEvenements();
 	return 0;
 }
+=======
+int main(int argc, char **argv) {
+
+	srand(time(NULL));
+	//a peut etre nelever de la 
+	GenMap();
+  initialiseGfx(argc, argv);
+  prepareFenetreGraphique("OpenGL", LargeurFenetre, HauteurFenetre);
+  lanceBoucleEvenements();
+  return 0;
+}
+
+void gestionEvenement(EvenementGfx evenement) {
+  static int start = 0;
+  static char map[32][29];
+  int taille = min(largeurFenetre(), hauteurFenetre()) / 32;
+  static Entity pac;
+  static Entity fantome;
+  static GameStat stat;
+  switch (evenement) {
+  case Initialisation: {
+    printf("Initialisation\n"); //////////////////////////////////
+    FillMap(map);
+    InitEntity(&pac, 1.5 * taille, 2.5 * taille, 4, 0, 0);
+    InitEntity(&fantome, 1.5 * taille, 22.5 * taille, 4, 2, 0);
+    demandeAnimation_ips(20); ////////////////////////////////////
+  } break;
+  case Affichage:
+    effaceFenetre(0, 0, 0);
+    if (start == 1) {
+      Map(map, 50, 50);
+      DrawPac(pac.x, pac.y, 50, 50, pac.d);
+      DrawFantome(fantome.x, fantome.y, 50, 50);
+>>>>>>> Stashed changes
 
 void gestionEvenement(EvenementGfx evenement){
 
