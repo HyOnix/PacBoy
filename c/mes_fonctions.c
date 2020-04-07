b23ab86a69b1c1dd642fa76a9d83422cf4246935
Quentin <qt1henry@gmail.com>
2020-04-07 15:02:19 +0200

    changes

--- c/mes_fonctions.c
+++ c/mes_fonctions.c
@@ -1,5 +1,4 @@
 #include "../h/mes_fonctions.h"
-<<<<<<< HEAD
 
 int rand_a_b(int a, int b)
 {
@@ -171,169 +170,6 @@ void FillMap(char coef[32][29])
   {
     for (size_t j = 0; j < 29; j++)
     {
-=======
-<<<<<<< Updated upstream
-=======
-
-
-int rand_a_b(int a, int b){
-        b++;
-        return rand()%(b-a)+a;
-}
-
-void GenMap(){
-FILE *ptrfile = fopen("file/GenMap.txt","w+");
-char coef[32][29];
-//debut
-for (int i = 0; i < 32; i++) {
-    for (int j = 0; j < 29; j++) {
-    	
-    		coef[i][j]='v';
-    
-    	
-    	}
-}
-//debut random
-int direc =0;
-int y =0;
-int x =0;
-int i=0;
-
-//au moins un horizontale et un verticale
-		x = rand_a_b(3,11);
-		for(int i= 1;i<15;i++){
-			coef[x][i]='.';
-			coef[x+1][i]='*';
-		}
-		y = rand_a_b(3,11);
-		for(int i= 1;i<15;i++){
-			coef[i][y]='*';
-			coef[i][y+1]='.';
-			coef[1][y]='.';
-		}
-while(i<2){
-	direc = rand_a_b(1,2);
-	switch(direc){
-		case 1://verti
-		
-		y = rand_a_b(3,11);
-		for(int i= 1;i<15;i++){
-			coef[i][y]='*';
-			coef[i][y+1]='.';
-			coef[1][y]='.';
-		}
-		break;
-		case 2: //hori
-		x = rand_a_b(3,11);
-		for(int i= 1;i<15;i++){
-			coef[x][i]='.';
-			coef[x+1][i]='*';
-		}
-		break;
-	
-	}
-	
-	
-	
-
-i++;
-}
-//copie des 1/4
-   
-for (int i = 1; i < 14 ; i++) {
-    for (int j = 1; j < 14 ; j++) {
-    coef[i][j+14]=coef[i][j];
-    
-    }
-   }
-for (int i = 1; i < 14 ; i++) {
-    for (int j = 1; j < 14 ; j++) {
-    coef[i+14][j]=coef[i][j];
-    
-    }
-   }
-   for (int i = 1; i < 14 ; i++) {
-    for (int j = 1; j < 14 ; j++) {
-    coef[i+14][j+14]=coef[i][j];
-    
-    }
-   }
-
-//bande horizontale tj presente
-for(int i = 0;i<29;i++){
-	coef[14][i]='.';
-	coef[15][i]='*';
-}
-coef[15][10]='.';
-
-//bande verticale tj presente
-for(int i = 0;i<32;i++){
-	coef[i][14]='*';
-	coef[i][15]='.';
-}
-coef[17][14]='.';
-coef[1][14]='.';
-
-
-//definition boite a phantome qui ne change pas
-for (int i = 13; i < 17; i++) {
-    for (int j = 11; j < 18; j++) {
-    		coef[i][j]='0';
-    	}
-}
-for (int i = 14; i < 16; i++) {
-    for (int j = 12; j < 17; j++) {
-    		coef[i][j]='8';
-    	}
-}
-coef[16][14]='8';
-
-//definition mur qui ne change pas
-for (int i = 0; i < 32; i++) {
-    for (int j = 0; j < 29; j++) {
-    	if((i == 0 || j==0 || i==31 || j==28) && (i!=14&&i!=15)){
-    		coef[i][j]='0';
-    	}
-    	
-    	}
-}
-
-//on finit par emplir tout les murs manquant
-for (int i = 0; i < 32; i++) {
-    for (int j = 0; j < 29; j++) {
-    if(coef[i][j]=='v'){
-    coef[i][j]='0';
-    }
-    }
-   }
-
-
-
-//on copie dans le fichier
-	for (int i = 0; i < 32; i++) {
-    for (int j = 0; j < 29; j++) {
-    		fprintf(ptrfile, "%c ",coef[i][j]);
-    }
-    fprintf(ptrfile, "\n");
-	}
-fclose(ptrfile);
-}
-
-
-
-
-
-int min(int a, int b) { return (a < b) ? a : b; }
-
-void FillMap(char coef[32][29]) {
-
-  FILE *ptrfile = fopen("file/map.txt", "r");
-  //pour tester les maps generer aleatoirement
-  //FILE *ptrfile = fopen("file/GenMap.txt", "r");
-  char init;
-  for (size_t i = 0; i < 32; i++) {
-    for (size_t j = 0; j < 29; j++) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
       fscanf(ptrfile, "%c ", &init);
       coef[i][j] = init;
     }
@@ -341,12 +177,8 @@ void FillMap(char coef[32][29]) {
   fclose(ptrfile);
 }
 
-<<<<<<< HEAD
 void Map(char map[32][29], int x, int y)
 {
-=======
-void Map(char map[32][29], int x, int y) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
   int mini = min(largeurFenetre(), hauteurFenetre());
   int taille = mini / 32;
   int decx = (largeurFenetre() - 29 * taille) * x / 100;
@@ -354,22 +186,15 @@ void Map(char map[32][29], int x, int y) {
   int cmp = 0, pos;
   int g = 0, b = 0, h = 0, d = 0;
   int list[4];
-<<<<<<< HEAD
   for (int i = 0; i < 32; i++)
   {
     for (int j = 0; j < 29; j++)
     {
-=======
-  for (int i = 0; i < 32; i++) {
-    for (int j = 0; j < 29; j++) {
-
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
       cmp = 0;
       g = 0;
       b = 0;
       h = 0;
       d = 0;
-<<<<<<< HEAD
       switch (map[i][j])
       {
       case '0':
@@ -387,20 +212,6 @@ void Map(char map[32][29], int x, int y) {
         }
         if (j - 1 >= 0 && map[i][j - 1] == '0')
         {
-=======
-      switch (map[i][j]) {
-      case '0':
-        if (i + 1 <= 32 && map[i + 1][j] == '0') {
-          g = 1;
-        }
-        if (i - 1 >= 0 && map[i - 1][j] == '0') {
-          d = 1;
-        }
-        if (j + 1 <= 29 && map[i][j + 1] == '0') {
-          b = 1;
-        }
-        if (j - 1 >= 0 && map[i][j - 1] == '0') {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
           h = 1;
         }
         // printf("%i\n",cmp);
@@ -409,7 +220,6 @@ void Map(char map[32][29], int x, int y) {
         list[2] = d;
         list[3] = h;
         cmp = list[0] + list[1] + list[2] + list[3];
-<<<<<<< HEAD
         switch (cmp)
         {
         case 1:
@@ -432,25 +242,6 @@ void Map(char map[32][29], int x, int y) {
           }
           if (b)
           {
-=======
-        switch (cmp) {
-        case 1:
-          couleurCourante(0, 0, 255);
-
-          if (g) {
-            arrondiBout(j * taille + decx + taille / 2,
-                        i * taille + decy + taille / 2, taille, 3);
-          }
-          if (d) {
-            arrondiBout(j * taille + decx + taille / 2,
-                        i * taille + decy + taille / 2, taille, 1);
-          }
-          if (h) {
-            arrondiBout(j * taille + decx + taille / 2,
-                        i * taille + decy + taille / 2, taille, 2);
-          }
-          if (b) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
             arrondiBout(j * taille + decx + taille / 2,
                         i * taille + decy + taille / 2, taille, 4);
           }
@@ -466,12 +257,8 @@ void Map(char map[32][29], int x, int y) {
           for (pos = 0; list[pos] == 2; pos++)
             ;
 
-<<<<<<< HEAD
           if (list[(pos + 1) % 4] && list[(pos + 3) % 4])
           {
-=======
-          if (list[(pos + 1) % 4] && list[(pos + 3) % 4]) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
             couleurCourante(0, 0, 255);
             rectangle(j * taille + decx, i * taille + decy,
                       (j + 1) * taille + decx, (i + 1) * taille + decy);
@@ -479,12 +266,8 @@ void Map(char map[32][29], int x, int y) {
             rectangle(j * taille + decx, i * taille + decy + 2,
                       (j + 1) * taille + decx, (i + 1) * taille + decy - 2);
           }
-<<<<<<< HEAD
           if (list[(pos + 2) % 4] && list[(pos + 4) % 4])
           {
-=======
-          if (list[(pos + 2) % 4] && list[(pos + 4) % 4]) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
             couleurCourante(0, 0, 255);
             rectangle(j * taille + decx, i * taille + decy,
                       (j + 1) * taille + decx, (i + 1) * taille + decy);
@@ -496,12 +279,8 @@ void Map(char map[32][29], int x, int y) {
           break;
         case 2:
           couleurCourante(0, 0, 255);
-<<<<<<< HEAD
           if (map[i + 1][j] == '0' && map[i - 1][j] == '0')
           {
-=======
-          if (map[i + 1][j] == '0' && map[i - 1][j] == '0') {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
             couleurCourante(0, 0, 255);
             rectangle(j * taille + decx, i * taille + decy,
                       (j + 1) * taille + decx, (i + 1) * taille + decy);
@@ -510,12 +289,8 @@ void Map(char map[32][29], int x, int y) {
                       (j + 1) * taille + decx - 2, (i + 1) * taille + decy);
             break;
           }
-<<<<<<< HEAD
           if (map[i][j + 1] == '0' && map[i][j - 1] == '0')
           {
-=======
-          if (map[i][j + 1] == '0' && map[i][j - 1] == '0') {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
             couleurCourante(0, 0, 255);
             rectangle(j * taille + decx, i * taille + decy,
                       (j + 1) * taille + decx, (i + 1) * taille + decy);
@@ -528,15 +303,10 @@ void Map(char map[32][29], int x, int y) {
           for (pos = 0; list[pos] == 1; pos++)
             ;
           // printf("%i\n",pos);
-<<<<<<< HEAD
           if (list[(pos + 1) % 4])
           {
             switch ((pos + 1) % 4)
             {
-=======
-          if (list[(pos + 1) % 4]) {
-            switch ((pos + 1) % 4) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
             case 0:
               // arrondiAngle(j*taille+decx+taille/2,i*taille+decy+taille/2,taille,
               // 4);
@@ -556,15 +326,10 @@ void Map(char map[32][29], int x, int y) {
               break;
             }
           }
-<<<<<<< HEAD
           if (list[(pos + 3) % 4])
           {
             switch ((pos + 3) % 4)
             {
-=======
-          if (list[(pos + 3) % 4]) {
-            switch ((pos + 3) % 4) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
             case 0:
               arrondiAngle(j * taille + decx + taille / 2,
                            i * taille + decy + taille / 2, taille, 2);
@@ -592,12 +357,8 @@ void Map(char map[32][29], int x, int y) {
                     (j + 1) * taille + decx, (i + 1) * taille + decy);
           break;
         }
-<<<<<<< HEAD
         if (cmp >= 4 || cmp < 0)
         {
-=======
-        if (cmp >= 4 || cmp < 0) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
           // printf("%i", cmp);
           couleurCourante(255, 0, 0);
           rectangle(j * taille + decx, i * taille + decy,
@@ -605,12 +366,8 @@ void Map(char map[32][29], int x, int y) {
 
           for (pos = 0; list[pos] == 1; pos++)
             ;
-<<<<<<< HEAD
           if (list[(pos + 1) % 4] && list[(pos + 3) % 4])
           {
-=======
-          if (list[(pos + 1) % 4] && list[(pos + 3) % 4]) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
             couleurCourante(0, 0, 0); // changer le test
             // couleurCourante(0, 255, 255);
             rectangle(j * taille + decx, i * taille + decy,
@@ -641,12 +398,8 @@ void Map(char map[32][29], int x, int y) {
   }
 }
 
-<<<<<<< HEAD
 void DrawPac(float x, float y, int dx, int dy, int direction)
 {
-=======
-void DrawPac(float x, float y, int dx, int dy, int direction) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
   int r = 2;
   int mini = min(largeurFenetre(), hauteurFenetre());
   int taille = mini / 32;
@@ -666,12 +419,8 @@ void DrawPac(float x, float y, int dx, int dy, int direction) {
   point(x, y);
   couleurCourante(0, 0, 0);
   taille--;
-<<<<<<< HEAD
   switch (count)
   {
-=======
-  switch (count) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
   case 0:
     tri_angle(x, y, 8, direction, taille);
     break;
@@ -688,17 +437,11 @@ void DrawPac(float x, float y, int dx, int dy, int direction) {
   count = state / r;
 }
 
-<<<<<<< HEAD
 void tri_angle(float x, float y, int ouverture, int direction, float taille)
 {
 
   switch (direction)
   {
-=======
-void tri_angle(float x, float y, int ouverture, int direction, float taille) {
-
-  switch (direction) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
   case 1: // left
     triangle(x, y, x - (taille / 2), y - (taille / ouverture), x - (taille / 2),
              y + (taille / ouverture));
@@ -721,12 +464,8 @@ void tri_angle(float x, float y, int ouverture, int direction, float taille) {
   }
 }
 
-<<<<<<< HEAD
 void InitEntity(Entity *ent, int x, int y, int v, int d, int s)
 {
-=======
-void InitEntity(Entity *ent, int x, int y, int v, int d, int s) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
   ent->x = x;
   ent->y = y;
   ent->v = v;
@@ -734,22 +473,14 @@ void InitEntity(Entity *ent, int x, int y, int v, int d, int s) {
   ent->state = s;
 }
 
-<<<<<<< HEAD
 int isOK(int x, int y, char coef[32][29])
 {
-=======
-int isOK(int x, int y, char coef[32][29]) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
   int mini = min(largeurFenetre(), hauteurFenetre());
   int taille = mini / 32;
   y /= taille;
   x /= taille;
-<<<<<<< HEAD
   if (coef[y][x] == ' ' || coef[y][x] == '*')
   {
-=======
-  if (coef[y][x] == ' ' || coef[y][x] == '*') {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
     return 1;
     // printf("ok");
   }
@@ -757,7 +488,6 @@ int isOK(int x, int y, char coef[32][29]) {
   return 0;
 }
 
-<<<<<<< HEAD
 void DeplacementPac(Entity *pac, char coef[32][29])
 {
   int mini = min(largeurFenetre(), hauteurFenetre());
@@ -769,44 +499,25 @@ void DeplacementPac(Entity *pac, char coef[32][29])
   case 3:
     if (isOK(pac->x + (pac->d - 2) * (pac->v + taille), pac->y, coef))
     {
-=======
-void DeplacementPac(Entity *pac, char coef[32][29]) {
-  int mini = min(largeurFenetre(), hauteurFenetre());
-  int taille = mini / 32;
-  taille = 2 + 5;
-  switch (pac->d) {
-  case 1:
-  case 3:
-    if (isOK(pac->x + (pac->d - 2) * (pac->v + taille), pac->y, coef)) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
       pac->x += (pac->d - 2) * (pac->v);
     }
     break;
   case 2:
   case 4:
-<<<<<<< HEAD
     if (isOK(pac->x, pac->y + (pac->d - 3) * (pac->v + taille), coef))
     {
-=======
-    if (isOK(pac->x, pac->y + (pac->d - 3) * (pac->v + taille), coef)) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
       pac->y += (pac->d - 3) * (pac->v);
     }
     break;
   }
 }
 
-<<<<<<< HEAD
 void DeplacementIA0(Entity *pac, char coef[32][29])
 {
-=======
-void DeplacementIA0(Entity *pac, char coef[32][29]) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
   int mini = min(largeurFenetre(), hauteurFenetre());
   int taille = mini / 32;
   taille = 2 + 5;
   int test;
-<<<<<<< HEAD
   do
   {
     switch (pac->d)
@@ -820,34 +531,18 @@ void DeplacementIA0(Entity *pac, char coef[32][29]) {
       }
       else
       {
-=======
-  do {
-    switch (pac->d) {
-    case 1:
-    case 3:
-      test = isOK(pac->x + (pac->d - 2) * (pac->v + taille), pac->y, coef);
-      if (test == 1) {
-        pac->x += (pac->d - 2) * (pac->v);
-      } else {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
         pac->d = rand() % 4 + 1;
       }
       break;
     case 2:
     case 4:
       test = isOK(pac->x, pac->y + (pac->d - 3) * (pac->v + taille), coef);
-<<<<<<< HEAD
       if (test == 1)
       {
         pac->y += (pac->d - 3) * (pac->v);
       }
       else
       {
-=======
-      if (test == 1) {
-        pac->y += (pac->d - 3) * (pac->v);
-      } else {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
         pac->d = rand() % 4 + 1;
       }
       break;
@@ -856,12 +551,8 @@ void DeplacementIA0(Entity *pac, char coef[32][29]) {
   } while (test != 1);
 }
 
-<<<<<<< HEAD
 void AfficheVie(int x, int y, GameStat stat)
 {
-=======
-void AfficheVie(int x, int y, GameStat stat) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
   char buf[10] = "VIE:";
   epaisseurDeTrait(2);
   int mini = min(largeurFenetre(), hauteurFenetre()) / 20;
@@ -872,12 +563,8 @@ void AfficheVie(int x, int y, GameStat stat) {
                 hauteurFenetre() * x / 100);
 }
 
-<<<<<<< HEAD
 void AffichageScore(int x, int y, GameStat stat)
 {
-=======
-void AffichageScore(int x, int y, GameStat stat) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
   char buf[10] = "SCORE:";
   epaisseurDeTrait(2);
   int mini = min(largeurFenetre(), hauteurFenetre()) / 20;
@@ -888,12 +575,8 @@ void AffichageScore(int x, int y, GameStat stat) {
                 hauteurFenetre() * x / 100);
 }
 
-<<<<<<< HEAD
 void Manger(Entity pac, GameStat *stat, int taille, char coef[32][29])
 {
-=======
-void Manger(Entity pac, GameStat *stat, int taille, char coef[32][29]) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
 
   int mini = min(largeurFenetre(), hauteurFenetre());
   taille = mini / 32;
@@ -908,12 +591,8 @@ void Manger(Entity pac, GameStat *stat, int taille, char coef[32][29]) {
   x += decx;
   y += decy;
 
-<<<<<<< HEAD
   if (coef[(y - decy) / taille][(x - decx) / taille] == '*')
   {
-=======
-  if (coef[(y - decy) / taille][(x - decx) / taille] == '*') {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
     coef[(y - decy) / taille][(x - decx) / taille] = ' ';
     stat->point++;
   }
@@ -922,19 +601,12 @@ void Manger(Entity pac, GameStat *stat, int taille, char coef[32][29]) {
   // point(x, y);
 }
 
-<<<<<<< HEAD
 void arrondiBout(int x, int y, int taille, int orientation)
 {
   epaisseurDeTrait(taille + 1);
   point(x, y);
   switch (orientation)
   {
-=======
-void arrondiBout(int x, int y, int taille, int orientation) {
-  epaisseurDeTrait(taille + 1);
-  point(x, y);
-  switch (orientation) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
   case 1: // haut
     rectangle(x - (taille / 2), y, x + (taille / 2), y - (taille / 2));
     couleurCourante(0, 0, 0);
@@ -964,19 +636,12 @@ void arrondiBout(int x, int y, int taille, int orientation) {
   point(x, y);
 }
 
-<<<<<<< HEAD
 void arrondiAngle(int x, int y, int taille, int orientation)
 {
   epaisseurDeTrait(taille + 1);
   point(x, y);
   switch (orientation)
   {
-=======
-void arrondiAngle(int x, int y, int taille, int orientation) {
-  epaisseurDeTrait(taille + 1);
-  point(x, y);
-  switch (orientation) {
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
   case 1:
     // bas/gauche
     rectangle(x, y - (taille / 2), x + (taille / 2),
@@ -1025,7 +690,3 @@ void arrondiAngle(int x, int y, int taille, int orientation) {
   epaisseurDeTrait(taille - 3);
   point(x, y);
 }
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
--- main.c
+++ main.c
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-
-<<<<<<< Updated upstream
->>>>>>> ab50734d9af911d4bc1a2931f7c6f842e4c16313
 #include "h/mes_fonctions.h"
 #include "h/fonctions_menu.h"
 
@@ -15,40 +10,6 @@ GenMap();
   lanceBoucleEvenements();
   return 0;
 }
-=======
-int main(int argc, char **argv) {
-
-	srand(time(NULL));
-	//a peut etre nelever de la 
-	GenMap();
-  initialiseGfx(argc, argv);
-  prepareFenetreGraphique("OpenGL", LargeurFenetre, HauteurFenetre);
-  lanceBoucleEvenements();
-  return 0;
-}
-
-void gestionEvenement(EvenementGfx evenement) {
-  static int start = 0;
-  static char map[32][29];
-  int taille = min(largeurFenetre(), hauteurFenetre()) / 32;
-  static Entity pac;
-  static Entity fantome;
-  static GameStat stat;
-  switch (evenement) {
-  case Initialisation: {
-    printf("Initialisation\n"); //////////////////////////////////
-    FillMap(map);
-    InitEntity(&pac, 1.5 * taille, 2.5 * taille, 4, 0, 0);
-    InitEntity(&fantome, 1.5 * taille, 22.5 * taille, 4, 2, 0);
-    demandeAnimation_ips(20); ////////////////////////////////////
-  } break;
-  case Affichage:
-    effaceFenetre(0, 0, 0);
-    if (start == 1) {
-      Map(map, 50, 50);
-      DrawPac(pac.x, pac.y, 50, 50, pac.d);
-      DrawFantome(fantome.x, fantome.y, 50, 50);
->>>>>>> Stashed changes
 
 void gestionEvenement(EvenementGfx evenement) {
   static int start = 0;
